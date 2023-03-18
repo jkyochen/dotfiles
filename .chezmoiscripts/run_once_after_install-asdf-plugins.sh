86ebb377b012e31a5bb1266961a6f1805dc55544
ChenZiQiang <ziqiang.chen@outlook.com>
2023-03-18 23:30:56 +0900

    Add asdf config

---
+++ .chezmoiscripts/run_once_after_install-asdf-plugins.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin add go https://github.com/kennyp/asdf-golang.git
+
+asdf install
